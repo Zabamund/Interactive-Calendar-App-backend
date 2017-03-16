f5297b3a90ff05a590d2d47e799f6e1cf33a0898
julie <annjulg@gmail.com>
2017-03-16 11:13:13 +0100

    version 5.0 updated with relationships and refractoring

---
+++ CalendApp_5.0/src/main/java/calendApp/repository/EventRepository.java
@@ -0,0 +1,23 @@
+package calendApp.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import calendApp.domain.Event;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+	public Event findById(Long id);
+
+	public Event findByEventName(String eventName);
+
+	public void deleteById(Long id);
+
+	public List<Event> findAll();
+
+	public List<Event> findAllEventsByDate(LocalDate date);
+}
